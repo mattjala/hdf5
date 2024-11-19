f1178568a382303741296686c328ba39b347af9d
Matthew Larson <matthewjlar@gmail.com>
2024-11-19 13:37:54 -0600

    Make dynamic operation registration threadsafe

--- src/H5VLint.c
+++ src/H5VLint.c
@@ -219,10 +219,6 @@ H5VL_init_phase2(void)
     if (H5VL__set_def_conn() < 0)
         HGOTO_ERROR(H5E_VOL, H5E_CANTSET, FAIL, "unable to set default VOL connector");
 
-#ifdef H5_HAVE_MULTITHREAD
-    /* Initialize the atomic dynamic optional operation table */
-    H5VL__init_opt_operation_table();
-#endif
 done:
     FUNC_LEAVE_NOAPI(ret_value)
 } /* end H5VL_init_phase2() */
