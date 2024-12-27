b2dfd771d017c84f4e3046838902ffe02afe09b6
Matthew Larson <matthewjlar@gmail.com>
2024-12-27 16:23:16 -0600

    Removed unused free lists from H5A

--- src/H5A.c
+++ src/H5A.c
@@ -25,7 +25,6 @@
 #include "H5CXprivate.h" /* API Contexts                             */
 #include "H5Eprivate.h"  /* Error handling                           */
 #include "H5ESprivate.h" /* Event Sets                               */
-#include "H5FLprivate.h" /* Free Lists                               */
 #include "H5Iprivate.h"  /* IDs                                      */
 #include "H5MMprivate.h" /* Memory management                        */
 #include "H5Opkg.h"      /* Object headers                           */
