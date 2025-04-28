81765effcf0b5443003b132ef3135a783a6dd2d1
Matthew Larson <matthewjlar@gmail.com>
2025-04-28 11:11:12 -0500

    Replace colon with generic path separator

--- tools/test/h5copy/CMakeTests.cmake
+++ tools/test/h5copy/CMakeTests.cmake
@@ -450,7 +450,7 @@
                 -D "TEST_EXPECT=${resultcode}"
                 -D "TEST_REFERENCE=./testfiles/${infile}.txt"
                 -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}:${vol_plugin_path}"
+                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}${CMAKE_SEP}${vol_plugin_path}"
                 -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
                 -P "${HDF_RESOURCES_DIR}/runTest.cmake"
         )
@@ -466,7 +466,7 @@
                 -D "TEST_EXPECT=${resultcode}"
                 -D "TEST_REFERENCE=./testfiles/${infile}.txt"
                 -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins:${vol_plugin_path}"
+                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins${CMAKE_SEP}${vol_plugin_path}"
                 -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
                 -P "${HDF_RESOURCES_DIR}/runTest.cmake"
         )
@@ -489,7 +489,7 @@
               -D "TEST_EXPECT=${resultcode}"
               -D "TEST_REFERENCE=./testfiles/${testname}.out.h5.txt"
               -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-              -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins:${vol_plugin_path}"
+              -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins${CMAKE_SEP}${vol_plugin_path}"
               -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
               -P "${HDF_RESOURCES_DIR}/runTest.cmake"
       )
@@ -547,7 +547,7 @@
                 -D "TEST_REFERENCE=./testfiles/${infile}_ERR.txt"
                 -D "TEST_MASK_ERROR=true"
                 -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}:${vol_plugin_path}"
+                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}${CMAKE_SEP}${vol_plugin_path}"
                 -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
                 -P "${HDF_RESOURCES_DIR}/runTest.cmake"
         )
@@ -564,7 +564,7 @@
                 -D "TEST_REFERENCE=./testfiles/${infile}_ERR.txt"
                 -D "TEST_MASK_ERROR=true"
                 -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins:${vol_plugin_path}"
+                -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins${CMAKE_SEP}${vol_plugin_path}"
                 -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
                 -P "${HDF_RESOURCES_DIR}/runTest.cmake"
         )
@@ -587,7 +587,7 @@
               -D "TEST_EXPECT=0"
               -D "TEST_REFERENCE=./testfiles/${testname}_ERR.out.h5.txt"
               -D "TEST_ENV_VAR=HDF5_PLUGIN_PATH"
-              -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins:${vol_plugin_path}"
+              -D "TEST_ENV_VALUE=${CMAKE_BINARY_DIR}/plugins${CMAKE_SEP}${vol_plugin_path}"
               -D "TEST_LIBRARY_DIRECTORY=${CMAKE_TEST_OUTPUT_DIRECTORY}"
               -P "${HDF_RESOURCES_DIR}/runTest.cmake"
       )
